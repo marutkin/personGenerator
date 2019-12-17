bb8206b89c6c3df8d73a8831b7c7d8a537a805de
Dmitriy Marutkin <dmitriy.marutkin@firstlinesoftware.com>
2019-12-17 15:28:03 +0300

    Some changes with port

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 
 const { PG } = require('./pg.js');
-const PORT = process.env.PORT || 5000
 
 // Require the framework and instantiate it
 const fastify = require('fastify')({ logger: true })
@@ -13,10 +12,8 @@ fastify.get('/', async (request, reply) => {
 // Run the server!
 const start = async () => {
   try {
-    await fastify.listen(PORT)
-    fastify.log.info(`server listening on ${fastify.server.address().port}`)
+    await fastify.listen(process.env.PORT, '0.0.0.0')
   } catch (err) {
-    fastify.log.error(err)
     process.exit(1)
   }
 }
