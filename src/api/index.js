66b438bb4b9f8dac336ec3e09121f74ee5358a0d
DMarutkin <dmitriy.marutkin@firstlinesoftware.com>
2020-01-29 16:21:13 +0300

    Add api methods.

--- src/api/index.js
+++ src/api/index.js
@@ -7,8 +7,28 @@ const fastify = require('fastify')();
 
 const localCache = PG.generate(100);
 
-fastify.register(require('fastify-cors'));
-fastify.get('/', async () => localCache).get('/generate', async () => PG.generate(100));
+fastify
+.register(require('fastify-cors'))
+.get('/', async (request) => {
+  const { from, to } = request.query;
+  if(from && to) {
+    const nFrom = Number(from);
+    const nTo = Number(to);
+    if(nFrom >= 0 && nTo <= 100) {
+      return localCache.slice(nFrom, nTo);
+    }
+  }
+  return localCache;
+})
+.get('/generate', async () => PG.generate(100))
+.get('/get/:count', async (request, reply) => {
+  const { count } = request.params;
+  if ( count && count > 0 && count <= 100 ) {
+    return localCache.slice(0, count);
+  }
+  return localCache;
+})
+.get('/help', async () => 'API commands are: 1) /get/:count (-- take data partialy ) 2) /generate (-- generate new list ) 3) /?from=value&to=value (-- slice data from one index to another and get it )');
 
 // Run the server!
 const start = async () => {
